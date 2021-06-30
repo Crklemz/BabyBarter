5e5e0cd9f6d0fe186b1728606c95e3fb1eb73c0c
Chris Klemz <crklemz@gmail.com>
2021-06-30 18:14:50 -0500

    user info printing to DOM in confirm claim page

--- src/components/ConfirmClaim/ConfirmClaim.jsx
+++ src/components/ConfirmClaim/ConfirmClaim.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import React, {useEffect} from 'react';
+import { useHistory } from "react-router-dom";
 
-function ConfirmClaim() {
+function ConfirmClaim({}) {
+
+    const dispatch = useDispatch();
+        const toys = useSelector(store => store.toys);
+        const user = useSelector(store => store.user);
+        const history = useHistory();
+
+        useEffect(() => {
+            dispatch({ type: 'FETCH_TOYS' });
+          }, [])
 
     return (
+        <>
         <p>confirm claim page</p>
+        <p>
+        {user.city} {user.email} {user.phone}
+        </p>
+        <div>
+            
+        </div>
+        </>
     )
 }
 
