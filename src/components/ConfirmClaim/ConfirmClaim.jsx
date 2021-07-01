d6dbf0d0886ff7f7597727c6000fa4d20a48094f
Chris Klemz <crklemz@gmail.com>
2021-07-01 10:40:28 -0500

    upon clicking claim toy button on home page, user is brought to the confirm claim page where the claimed toy info is successfully printing to the DOM

--- src/components/ConfirmClaim/ConfirmClaim.jsx
+++ src/components/ConfirmClaim/ConfirmClaim.jsx
@@ -1,13 +1,20 @@
 import {useDispatch, useSelector} from 'react-redux';
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import { useHistory } from "react-router-dom";
 
-function ConfirmClaim({}) {
 
-    const dispatch = useDispatch();
+
+function ConfirmClaim() {
+
+        const dispatch = useDispatch();
         const toys = useSelector(store => store.toys);
         const user = useSelector(store => store.user);
+        const claimed = useSelector(store => store.claimed);
         const history = useHistory();
+        
+
+        // const showClaimed = () => {
+        //     const claimedToyId = claimed;
 
         useEffect(() => {
             dispatch({ type: 'FETCH_TOYS' });
@@ -17,11 +24,22 @@ function ConfirmClaim({}) {
         <>
         <p>confirm claim page</p>
         <p>
-        {user.city} {user.email} {user.phone}
+        {user.city} {user.email} {user.phone} {claimed} 
         </p>
+
         <div>
-            
+            {toys.filter(toy => toy.id == claimed).map(filteredToy => (
+                <li key={filteredToy.id}>
+                <p>Toy Title: {filteredToy.title}</p>
+                <p>Description: {filteredToy.description}</p>
+                <p>For ages: {filteredToy.age}+</p>
+                <p>Condition: {filteredToy.condition}</p>
+            <img src={filteredToy.image_url} alt={filteredToy.description} width="100px" height="100px"/>
+            <button>Confirm Claim</button>
+                </li>
+            ))}
         </div>
+        
         </>
     )
 }
